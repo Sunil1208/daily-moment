7d0a28e83eae2656ca9ee6a81256fda42548ba79
sunilk1208 <sunil.k@goodbox.in>
2021-02-25 22:21:32 +0530

    Project initialised

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import {
+  IonApp
+} from '@ionic/react';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SettingsPage from './pages/SettingsPage';
+
+
+
+const App: React.FC = () => {
+  return (
+    <IonApp>
+      <BrowserRouter>
+        <Route exact path="/home">
+          <HomePage />
+        </Route>
+        <Route exact path="/settings">
+          <SettingsPage />
+        </Route>
+      </BrowserRouter>
+    </IonApp>
+  );
+};
+
+export default App;

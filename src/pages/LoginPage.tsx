88fa685b587becca06558f66f419a7a79933a6c0
sunilk1208 <sunil.k@goodbox.in>
2021-03-09 21:52:00 +0530

    loader, handling errors

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,7 +8,9 @@ import {
   IonList,
   IonItem,
   IonLabel,
-  IonInput
+  IonInput,
+  IonText,
+  IonLoading
 } from '@ionic/react';
 import React,  { useState } from 'react';
 import { Redirect } from 'react-router';
@@ -22,13 +24,30 @@ const LoginPage: React.FC<Props> = ({ onLogin }) => {
   const { loggedIn } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+  const [status, setStatus] = useState({
+    loading: false,
+    error: false,
+    errorMessage: ''
+  })
+
   const handleLogin = async () => {
     console.log('should login with', {email, password})
-    // const credential =  auth.signInWithEmailAndPassword("test1@example.org", "123456")
-    const credential =  auth.signInWithEmailAndPassword(email, password)
-    console.log('credential', credential);
-    onLogin();
+    try {
+      // "test1@example.org", "123456"
+      setStatus({...status, loading: true, error: false});
+      const credential = await auth.signInWithEmailAndPassword(email, password)
+      console.log('credential', credential);
+      setStatus({...status, loading: false, error: false});
+      onLogin();
+    } catch (error) {
+      console.log('error', error)
+      setStatus({...status, loading: false, error: true, errorMessage: error.message});
+      let timer = setTimeout(() => {
+        setStatus({...status, loading: false, error: false, errorMessage: ''});
+      }, 5000);
+      clearTimeout(timer);
+    }
+    
   }
   if(loggedIn){
     return <Redirect to="/my/entries" />;
@@ -57,7 +76,11 @@ const LoginPage: React.FC<Props> = ({ onLogin }) => {
             />
           </IonItem>
         </IonList>
+        {status.error && (
+          <IonText color="danger" >{status.errorMessage} &nbsp;</IonText>
+        )}
         <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
+        <IonLoading isOpen={status.loading} />
       </IonContent>
     </IonPage>
   );

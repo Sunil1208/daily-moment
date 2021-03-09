ccd5fa297c1e540c03255aa81bd8ccbca2db7f62
sunilk1208 <sunil.k@goodbox.in>
2021-03-09 21:22:22 +0530

    handling login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -4,18 +4,32 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  IonButton
+  IonButton,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput
 } from '@ionic/react';
-import React from 'react';
+import React,  { useState } from 'react';
 import { Redirect } from 'react-router';
 import { useAuth } from '../auth';
-
+import { auth } from '../firebase'
 
 interface Props {
   onLogin: () => void;
 }
 const LoginPage: React.FC<Props> = ({ onLogin }) => {
   const { loggedIn } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const handleLogin = async () => {
+    console.log('should login with', {email, password})
+    // const credential =  auth.signInWithEmailAndPassword("test1@example.org", "123456")
+    const credential =  auth.signInWithEmailAndPassword(email, password)
+    console.log('credential', credential);
+    onLogin();
+  }
   if(loggedIn){
     return <Redirect to="/my/entries" />;
   }
@@ -27,7 +41,23 @@ const LoginPage: React.FC<Props> = ({ onLogin }) => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonButton expand="block" onClick={onLogin}>Login</IonButton>
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Email</IonLabel>
+            <IonInput 
+              type="email" 
+              onIonChange={e => {setEmail(e.detail.value)}}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Password</IonLabel>
+            <IonInput 
+              type="password"
+              onIonChange={e => {setPassword(e.detail.value)}}
+            />
+          </IonItem>
+        </IonList>
+        <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
       </IonContent>
     </IonPage>
   );

23373e07a63e8a94ec44000a85fcf2369c3cd7df
sunilk1208 <sunil.k@goodbox.in>
2021-03-20 16:00:37 +0530

    initializing firebase storage service

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const firebaseConfig = {
 
   export const auth =  app.auth();
   export const firestore = app.firestore();
+  export const storage = app.storage();
